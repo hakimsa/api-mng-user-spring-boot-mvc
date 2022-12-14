6dd6fc3d6f70eb172187912aad19bd5a5612b364
hakim <samouh591@gmail.com>
2022-12-14 23:32:57 +0100

    modif

--- src/main/java/com/hakimsamouh/mgtapp/Users/Controllers/UserController.java
+++ src/main/java/com/hakimsamouh/mgtapp/Users/Controllers/UserController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +20,7 @@ import com.hakimsamouh.mgtapp.Users.Models.User;
 import com.hakimsamouh.mgtapp.Users.Services.UserService;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 @RequestMapping(path = "/api/v1/users")
 public class UserController {
 
