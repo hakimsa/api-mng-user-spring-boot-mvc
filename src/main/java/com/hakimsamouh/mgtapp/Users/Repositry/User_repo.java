2db0f519f54e567f628cec5d489fd5dbd50a54a6
hakim <samouh591@gmail.com>
2022-12-19 13:29:52 +0100

    HT-1 <message>

--- src/main/java/com/hakimsamouh/mgtapp/Users/Repositry/User_repo.java
+++ src/main/java/com/hakimsamouh/mgtapp/Users/Repositry/User_repo.java
@@ -12,7 +12,6 @@ import jakarta.persistence.LockModeType;
 @Repository
 public interface  User_repo extends JpaRepository<User, Long>{
 @Lock(LockModeType.OPTIMISTIC_FORCE_INCREMENT) Optional<User> findWithLockingById(Long id); 
-
 //al method en jpa  en this interface
 //@Query("SELECT e FROM User e WHERE s.email=?1")
 Optional <User> findUserByEmail(String email);
